f9e0e006ab181dff0f3d9e7cbc8245434883dbf1
zouyonghao <yonghaoz1994@gmail.com>
2016-05-05 09:40:54 +0800

    change name

---
+++ reverse_list.cpp
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+struct List {
+    int value;
+    List *next;
+};
+
+int main() {
+    List head;
+    head.value = -1;
+    int n;
+    std::cin >> n;
+    List *tmp = &head;
+    for (int i = 0; i < n; i++) {
+        List *a = (List *) malloc(sizeof(List));
+        a->value = i;
+        a->next = NULL;
+        tmp->next = a;
+        tmp = tmp->next;
+    }
+    tmp = &head;
+    while (tmp->next != NULL) {
+        printf("%d\n", tmp->value);
+        tmp = tmp->next;
+    }
+
+    tmp = &head;
+    List *newNext = NULL;
+    List *oldNext = tmp->next;
+    while (oldNext != NULL) {
+        tmp->next = newNext;
+        newNext = tmp;
+        tmp = oldNext;
+        oldNext = oldNext->next;
+    }
+    
+    tmp->next = newNext;
+    while (tmp != NULL) {
+        printf("%d\n", tmp->value);
+        tmp = tmp->next;
+    }
+}
