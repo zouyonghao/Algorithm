0dcabc156febdef51b08c0956242528326cbc328
zouyonghao <yonghaoz1994@gmail.com>
2018-08-26 23:02:54 +0800

    189

--- 189.rotate-array.cpp
+++ 189.rotate-array.cpp
@@ -37,9 +37,22 @@
  * 
  * 
  */
+#include <vector>
+using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
+        if (k > nums.size()) {
+            k = k % nums.size();
+        }
+        for (int i = 0; i < nums.size() / 2; i++) {
+            swap(nums[i], nums[nums.size() - 1 - i]);
+        }
+        for (int i = 0; i < k / 2; i++) {
+            swap(nums[i], nums[k - 1 - i]);
+        }
+        for (int i = k; i < k + (nums.size() - k) / 2; i++) {
+            swap(nums[i], nums[nums.size() - 1 - (i - k)]);
+        }
     }
 };
